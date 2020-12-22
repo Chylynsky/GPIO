1cf691db9e756d4ec40c23d125320abb63cb4181
Borys Chylinski <borchy97@gmail.com>
2020-12-22 19:27:54 +0100

    [lib] Update example program.

--- GPIOtest/main.cpp
+++ GPIOtest/main.cpp
@@ -4,26 +4,47 @@
 
 #include "gpio.h"
 
-using namespace rpi;
-using namespace std;
-using namespace std::chrono;
-using namespace std::chrono_literals;
-using namespace std::this_thread;
-
 int main()
 {
-    // Declare GPIO pin attached to the LED as output.
-    gpio<dir::output> pinLed{ 26U };
+    using namespace rpi;
+    using namespace std;
+    using namespace std::chrono;
+    using namespace std::chrono_literals;
+    using namespace std::this_thread;
 
-    for (int i = 0; i < 10; i++)
+    // Declare GPIO pin attached to the LED as output.
+    gpio<dir::output> pinLED{ 26U };
+    // Declare GPIO pin attached to the button as input
+    gpio<dir::input> pinButton{ 26U };
+    // Set pull up
+    pinButton.set_pull(pull::up);
+
+    for (int i = 0; i < 2; i++)
     {
-        pinLed = HIGH;      // Use optimized assignment operator.
+        pinLED = HIGH;      // Use optimized assignment operator.
         sleep_for(200ms);
-        pinLed = false;     // Use standard assignment operator.
+        pinLED = false;     // Use standard assignment operator.
         sleep_for(200ms);
-        pinLed.write(1);    // Use function to assign value. Value must be convertible to bool.
+        pinLED.write(1);    // Use function to assign value. Value must be convertible to bool.
         sleep_for(200ms);
-        pinLed = 0;
+        pinLED = 0;
+        sleep_for(200ms);
+    }
+
+    cout << "Press the button!" << endl;
+
+    for (int i = 0; i < 500; i++)
+    {
+        if (pinButton.read() == 0U)
+        {
+            pinLED = HIGH;
+        }
+        else
+        {
+            pinLED = LOW;
+        }
+
+        sleep_for(20ms);
     }
 
     return 0;
